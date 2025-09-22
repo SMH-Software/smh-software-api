c3f0ab7dd50bb05b05b7c4f18b3938f087e8d1c2
SMH-Software <77654418+SMH-Software@users.noreply.github.com>
2025-09-22 16:41:19 +0000

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-     'allowed_origins' => [
+    'allowed_origins' => [
         'http://127.0.0.1:5500',             // ⚡ Ton front local
         'https://smh-software-api.vercel.app', // ⚡ Ton API sur Vercel
     ],
