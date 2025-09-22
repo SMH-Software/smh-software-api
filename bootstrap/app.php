1d435b9eb9859ff915c78c1136c1a5f3b319cb9c
SMH-Software <77654418+SMH-Software@users.noreply.github.com>
2025-09-22 16:37:23 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,15 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append([
-            HandleCors::class,
-        ]);
-
-        $middleware->group('api', [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ]);
+        $middleware->append([HandleCors::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
