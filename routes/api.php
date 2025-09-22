c66acd18f0084f6aaa7f54b9cdf2a1e55a5656b1
SMH-Software <77654418+SMH-Software@users.noreply.github.com>
2025-09-22 13:04:50 +0000

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,10 @@ use Illuminate\Support\Facades\Route;
 /* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum'); */
-Route::get('/', function(){
-    return 'SMH-SOFTWARE API';
-});
 
-Route::prefix('accounts')->name('account')->group(function (){
-    Route::get('/', [AccountController::class, 'index'])->name('index');
-    Route::post('/', [AccountController::class, 'store']);
-    Route::get('/{id}', [AccountController::class, 'show']);
-    Route::patch('/{id}', [AccountController::class, 'update']);
-    Route::delete('/{id}', [AccountController::class, 'delete']);
-});
+Route::get('/', [AccountController::class, 'index']);
+Route::post('/', [AccountController::class, 'store']);
+Route::get('/{id}', [AccountController::class, 'show']);
+Route::patch('/{id}', [AccountController::class, 'update']);
+Route::delete('/{id}', [AccountController::class, 'delete']);
+
