25d73baa250e9f4a7cd4bf870364c518caf9e58c
SMH-Software <77654418+SMH-Software@users.noreply.github.com>
2025-09-22 16:34:55 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->global([
-            \App\Http\Middleware\HandleCors::class,
+        $middleware->append([
+            HandleCors::class,
         ]);
 
         $middleware->group('api', [
